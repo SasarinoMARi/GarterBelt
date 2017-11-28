6f644b89e83f564f60970138e2420d106fdfe6df
Usagination <tjddlf2645@gmail.com>
2017-11-28 16:43:58 +0900

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GarterBelt
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		int pId = 0;
+		GlobalHotkeyBinder binder = new GlobalHotkeyBinder();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			LoadHandle();
+
+			binder.AddHindHandler(Keys.F1, ModifierKeys.Shift, delegate (object sender, KeyPressedEventArgs e)
+			{
+				HideByHandle();
+			});
+			binder.AddHindHandler(Keys.F2, ModifierKeys.Shift, delegate (object sender, KeyPressedEventArgs e)
+			{
+				ShowByHandle();
+			});
+		}
+
+		private void FindProcessID(int id)
+		{
+			this.pId = id;
+			this.labelHandleId.Content = "PID : " + id;
+		}
+
+		private void FindHandle()
+		{
+			Process[] processRunning = Process.GetProcesses();
+			foreach (Process p in processRunning)
+			{
+				if (p.ProcessName.ToLower().Contains("ffxiv"))
+				{
+					FindProcessID(p.MainWindowHandle.ToInt32());
+				}
+			}
+		}
+
+		private void ShowByHandle()
+		{
+			if (pId == 0) return;
+			WindowAnnotation.ShowWindow(pId, WindowAnnotation.WindowState.SW_SHOW);
+		}
+
+		private void HideByHandle()
+		{
+			if (pId == 0) return;
+			WindowAnnotation.ShowWindow(pId, WindowAnnotation.WindowState.SW_HIDE);
+		}
+
+		private void ExportHandle()
+		{
+			File.WriteAllText("id.txt", pId.ToString());
+		}
+
+		private void LoadHandle()
+		{
+			var temp = File.ReadAllText("id.txt");
+			if (temp == null) return;
+
+			int id = 0;
+			int.TryParse(temp, out id);
+			if (id == 0) return;
+
+			FindProcessID(id);
+		}
+
+
+		private void button_Click(object sender, RoutedEventArgs e )
+		{
+			if (sender == this.buttonFindHandle) FindHandle();
+
+			if (sender == this.buttonLoadHandle) LoadHandle();
+			if (sender == this.buttonExportHandle) ExportHandle();
+
+			if (sender == this.buttonHideByHandle) HideByHandle();
+			if (sender == this.buttonShowByHandle) ShowByHandle();
+		}
+	}
+}
---
+++ WindowAnnotation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarterBelt
+{
+	class WindowAnnotation
+	{
+		public enum WindowState { SW_HIDE = 0, SW_SHOWNORMAL = 1, SW_NORMAL = 1, SW_SHOWMINIMIZED = 2, SW_SHOWMAXIMIZED = 3, SW_MAXIMIZED = 3, SW_SHOWNOACTIVE = 4, SW_SHOW = 5, SW_MINIMIZED = 6, SW_SHOWMININOACTIVE = 7, SW_SHOWNA = 8, SW_RESTORE = 9, SW_SHOWDEFAULT = 10, SW_FORCEMINIMIZED = 11, SW_MAX = 11 }
+
+		[DllImport("User32")]
+		public static extern int ShowWindow(int hwnd, WindowState nCmdShow);
+	}
+}
